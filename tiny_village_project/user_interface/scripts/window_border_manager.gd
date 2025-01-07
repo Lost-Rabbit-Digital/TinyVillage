f5a09b32ed8190a6a73eb294940f2f4497a116c7
Boden McHale <bodenkaikarden@gmail.com>
2025-01-07 13:56:44 -0500

    Added animation to window border

---
+++ tiny_village_project/user_interface/scripts/window_border_manager.gd
@@ -0,0 +1,26 @@
+extends TextureRect
+
+var animation = {
+	"timer": 0.0,
+	"rate": 0.30,
+	"current_frame": 0,
+	"frames": []
+}
+
+func _ready():
+	# Load the textures
+	for i in range(4):
+		var texture = load("assets/window_borders/window_border_%d.png" % (i + 1))
+		animation.frames.append(texture)
+	
+	texture = animation.frames[0]
+
+func _process(delta):
+	animation.timer += delta
+	if animation.timer > animation.rate:
+		animation.timer = 0
+		set_new_frame()
+
+func set_new_frame():
+	animation.current_frame = (animation.current_frame + 1) % animation.frames.size()
+	texture = animation.frames[animation.current_frame]
